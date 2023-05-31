888f98e454f64bd66cee086c912dc66719354cba
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-05-31 18:29:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
-
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+import Input from './Form.js';
 
 const App = () => {
+  let [name,setName] = useState('default');
+  let [email,setEmail] = useState('');
+  let [password,setPassword] = useState('');
+
+  function submitting(e){
+    e.preventDefault();
+    let obj = {
+      name,
+      email,
+      password
+    };
+    console.log(obj);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <form onSubmit={submitting}>
+          <Input type={"text"} label={"Name"} placeholder={"enter name"} updateName={setName}/>
+          <Input type={"email"} label={"Email"} placeholder={"enter email"} updateName={setEmail}/>
+          <Input type={"password"} label={"Password"} placeholder={"enter password"} updateName={setPassword}/>
+          <button type="submit">Submit</button>
+        </form>
     </div>
   )
 }
