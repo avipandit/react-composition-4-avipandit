136444e308423c0ad3490fc31527fddfd6353b62
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-06-09 21:09:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-
-import React from "react";
-import './../styles/App.css';
+import React from 'react';
+import Form from './Form';
 
 const App = () => {
+  const handleSubmit = (formValues) => {
+    console.log(formValues);
+    // Perform any necessary actions with the form values
+  };
+
+  const fields = [
+    { name: 'name', type: 'text', label: 'Name' },
+    { name: 'email', type: 'email', label: 'Email' },
+    { name: 'password', type: 'password', label: 'Password' },
+  ];
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Form Example</h1>
+      <Form onSubmit={handleSubmit} fields={fields} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
